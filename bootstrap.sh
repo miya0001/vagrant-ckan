1686a901d959514fd7051d5d2ae201130cd1cf83
miya0001 <miya@wpist.me>
2015-02-09 16:31:15 +0900

    ver 2.3b

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,6 +24,7 @@ set -ex
 
 # Install the required packages
 
+apt-get update
 apt-get install -y nginx apache2 libapache2-mod-wsgi python-dev postgresql libpq-dev python-pip python-virtualenv git-core solr-jetty openjdk-6-jdk python-pastescript pwgen
 
 PASSWD=`pwgen 12 1`
@@ -38,7 +39,7 @@ chown `whoami` /usr/lib/ckan/default
 virtualenv --no-site-packages /usr/lib/ckan/default
 . /usr/lib/ckan/default/bin/activate
 
-pip install -e 'git+https://github.com/okfn/ckan.git@ckan-2.2#egg=ckan'
+pip install -e 'git://github.com/ckan/ckan.git@release-v2.3#egg=ckan'
 pip install -r /usr/lib/ckan/default/src/ckan/requirements.txt
 deactivate
 . /usr/lib/ckan/default/bin/activate
@@ -69,7 +70,7 @@ if [ -f /etc/solr/conf/schema.xml ]; then
 fi
 
 if [ ! -L /etc/solr/conf/schema.xml ]; then
-    ln -s /usr/lib/ckan/default/src/ckan/ckan/config/solr/schema-2.0.xml /etc/solr/conf/schema.xml
+    ln -s /usr/lib/ckan/default/src/ckan/ckan/config/solr/schema-2.3.xml /etc/solr/conf/schema.xml
 fi
 
 sudo service jetty restart
@@ -103,5 +104,5 @@ service nginx restart
 # echo postfix postfix/mail_name string $HOSTNAME | debconf-set-selections
 # apt-get -y install postfix
 
-sudo install -o root -g root -m 0600 /vagrant/files/ckan.cron /etc/cron.d/ckan
-sudo install -o root -g root -m 0700 /vagrant/files/reset_ckan_pass.sh /usr/local/bin/
+# sudo install -o root -g root -m 0600 /vagrant/files/ckan.cron /etc/cron.d/ckan
+# sudo install -o root -g root -m 0700 /vagrant/files/reset_ckan_pass.sh /usr/local/bin/
